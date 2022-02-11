b9f08072e30bdb8f8db18497ac1589cc68e5d0e6
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-11 22:40:27 +0000

    Commit of static and dynamic testing code and screenshots

---
+++ Static/Static_Testing.py
@@ -0,0 +1,23 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    # if card.value = 1: //card.value, should read card1.value
+      return True
+    # else //missing':' after 'else'
+      return False
+   
+
+  # dif highest_card(self, card1 card2): //'dif' should be 'def' and comma missing between card1 & card2
+  # if card1.value > card2.value: //indentation error
+    # return card // 'card' should read 'card1'
+  else: 
+    return card2
+  
+
+
+def cards_total(self, cards):
+  # total //word 'total' not required 
+  for card in cards:
+    total += card.value
+    # return "You have a total of" + total
